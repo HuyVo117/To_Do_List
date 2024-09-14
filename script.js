f9f0ee0ba87b9ee8e493ee17144ba76e7f17913f
BlackNoir <nhathuy1172005@gmai.com>
2024-09-14 11:34:43 +0700

    add button clear , in array when add a new text will genarate a new textarea

--- script.js
+++ script.js
@@ -6,19 +6,43 @@ document.querySelector("#add").addEventListener('click', () => {
   const item = document.querySelector("#item").value; 
   createItem(item);
 });
+document.querySelector("#clear").addEventListener('click', () => {
+clearItems();
+});
 
 function createItem(item){
     itemsArray.push(item);  
     localStorage.setItem('items', JSON.stringify(itemsArray));
     location.reload();  
 }
+function clearItems() {
+  itemsArray.length =0; // clear the array
+  localStorage.setItem('items', JSON.stringify(itemsArray)); // save the array
+  location.reload(); // reload the page
+  
+}
 
 function displayItems() {
-  let items = "";
+  let items = '';
   for (let index = 0; index < itemsArray.length; index++) {
-    
+    items += `
+    <div class="item">
+    <div class="input-controller">
+        <textarea disabled> ${itemsArray[index]} </textarea>
+        <div class="edit-controller">
+            <i class="fa-solid fa-check deleteBtn"></i>
+            <i class="fa-regular fa-pen-to-square editBtn"></i>
+        </div>
+    </div>
+    <div class="update-controller">
+        <button class="saveBtn">Save</button>
+        <button class="cancelBtn">Cancel</button>
+    </div>
+</div>
+`;
     
   }
+  document.querySelector(".to_do_list").innerHTML = items;
   
 }
 function displayDay() {
@@ -27,4 +51,7 @@ function displayDay() {
   document.querySelector("#date").innerHTML = date[0] + " " + date[1] + " " + date[2] + " " + date[3];
 }
 
-window.onload = displayDay;
+window.onload = function() {
+  displayItems();
+  displayDay();
+}
